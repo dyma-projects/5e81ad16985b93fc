310254899f3e51ef28c1a06ebc18cda967d955d6
Marc Chassoulier <marc@chassoulier.com>
2020-05-27 21:07:20 +0200

    Projet 5

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class UserService {
+  public users: BehaviorSubject<string[]> = new BehaviorSubject([]);
+
+  constructor() { }
+
+  public addUser(user :string ) :void{
+    this.users.value.push(user);
+  }
+
+}
